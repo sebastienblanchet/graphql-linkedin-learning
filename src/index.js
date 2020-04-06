adcbf16cdbc1d87de1715bfb339747df8cf4ca97
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-04-06 11:06:24 -0400

    Initial GraphQL setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from './schema';
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('GraphQL is amazing!');
+  res.send('GraphQL is amazing!');
 });
+
+// resolver for grapgql, fxn that returns data
+const root = {
+  hello: () => "Hi, I'm Manny"
+};
+
+// use similar to express
+app.use('/graphql', graphqlHTTP({
+  // import from buildScheme
+  schema: schema,
+  // resolver
+  rootValue: root,
+  graphiql: true,
+}))
 
 app.listen(8080, () => console.log('Running server on port localhost:8080/graphql'));
